0d09b6789a76cf72236f13a32f23d412c19098fd
Razvan <popescu.razvanmihai94@gmail.com>
2023-01-23 23:56:56 +0200

    new version

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit =async (e) => {
 
   //fetching response from the server
 
-  const response = await fetch('https://codexchatbot-jr6e.onrender.com/',{
+  const response = await fetch('https://codexchatbot-jr6e.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
